a2cea35c4fd892ef27f6bdbd1781380172ff4ce7
Mario Paucar <ingmariopaucar@gmail.com>
2024-12-17 22:07:33 -0500

    dia3

---
+++ BDS--G3/Modulo_1/Dia_1.3/01_listas.py
@@ -0,0 +1,23 @@
+dias=['lunes','martes','miercoles']
+print(dias)
+print(dias[-2])
+print(dias[1:3])
+
+
+#agregar valores a la lista - APPEND
+dias.append('jueves')
+dias.append('viernes')
+print(dias)
+
+#Eliminar un valor de la lista
+dias.pop(3)
+print(dias)
+del dias[2:4]
+print(dias)
+dias.append('miercoles')
+dias.append('jueves')
+
+#actualizar un valor
+dias[2]='act'
+print(dias)
+
